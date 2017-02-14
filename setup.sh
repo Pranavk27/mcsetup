deec60108ecda5273b92eeb7bad3038b6b0cdf9a
root <root@localhost.localdomain>
2017-02-13 23:30:31 -0500

    @SETUP - code review changes

---
+++ setup.sh
@@ -0,0 +1,36 @@
+VIM=vim
+PLGINSTALLOPTS=" +PluginInstall +qall"
+VIMRC=/etc/vimrc
+PLUGINLIST="omz_zsh tpowerline ycmd font"
+CONFIGPATH="$HOME/myscripts/config"
+PACKAGEFILE="$HOME/myscripts/packages"
+
+source $HOME/myscripts/setupvim.sh
+
+function package_present()
+{
+				[ $? -ne 0 ]
+}
+
+function packages_install()
+{
+				for PACKAGE in $PACKAGES;
+				do
+				echo ">> Check $PACKAGE is present <<"
+				which $PACKAGE
+				if package_present;
+				then
+								echo ">> $PACKAGE not present >> Installing <<"
+								yum -y install $PACKAGE;
+								echo ">> $PACKAGE now present <<"
+				fi
+				done
+}
+
+echo "Installing Packages"
+packages_install `cat $PACKAGEFILE` 
+packages_install vim  
+setup_vimrc
+mkdir $HOME/.vim
+setup_plugins
+for_each_plugin
--- setupvim.sh
+++ setupvim.sh
@@ -1,18 +1,15 @@
-VIM=vim
-PLGINSTALLOPTS=" +PluginInstall +qall"
-VIMRC=/etc/vimrc
-PLUGINLIST="tpowerline ycmd font"
-CONFIGPATH="$HOME/myscripts/config"
-
-
 function font_install()
 {
+				rm -rf $HOME/fonts/*
 				git clone https://github.com/powerline/fonts.git $HOME/fonts
 				cd $HOME/fonts
 				./install.sh
+				rm -rf $HOME/fonts/*
 				git clone https://github.com/belluzj/fantasque-sans.git $HOME/fonts
 				cd $HOME/fonts
-				make &&	make &&	make &&	make &&	make
+				make ||	make ||	make ||	make ||	make
+				cp *.ttf $HOME/.local/share/fonts
+				rm -rf $HOME/fonts
 }
 
 function tpowerline_install()
@@ -20,29 +17,35 @@ function tpowerline_install()
 				cd $HOME/.vim/bundle/tmux-powerline
 				./generate_rc.sh
 				mv $HOME/.tmux-powerlinerc.default $HOME/.tmux-powerlinerc
-				cd -
+}
+
+function omz_zsh_install()
+{
+				cd $HOME
+				sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 function clang_install()
 {
-				cd $HOME
-				svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-				cd llvm/tools
-				svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-				cd ../..
-				cd llvm/tools/clang/tools
-				svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
-				cd ../../../..
-				cd llvm/projects
-				svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-				cd ../..
-				cd llvm/projects
-				svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-				cd ../..
-				mkdir build
-				cd build
-				cmake -G "Unix Makefiles" ../llvm
-				make
+#				cd $HOME
+#				svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+#				cd llvm/tools
+#				svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+#				cd ../..
+#				cd llvm/tools/clang/tools
+#				svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+#				cd ../../../..
+#				cd llvm/projects
+#				svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+#				cd ../..
+#				cd llvm/projects
+#				svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+#				cd ../..
+#				mkdir build
+#				cd build
+#				cmake -G "Unix Makefiles" ../llvm
+#				make
+			  packages_install llvm clang	
 }
 
 function ycmd_install()
@@ -51,42 +54,27 @@ function ycmd_install()
 				cd $HOME/.vim/bundle/YouCompleteMe
 				git submodule update --init --recursive
 				./install.py --all
-				cd $HOME
-				mkdir ycm_build
-				cd ycm_build
+				rm -rf $HOME/ycm_build
+				mkdir $HOME/ycm_build
+				cd $HOME/ycm_build
 				cmake -G 'Unix Makefiles' -DPATH_TO_LLVM_ROOT=$HOME/llvm $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 				cmake --build . --target ycm_core --config Release
-}
-
-function install_vim()
-{
-				echo ">> Check VIm is present <<"
-				if ! vim_present;
-				then
-								echo ">> VIm not present >> Installing <<"
-								yum -y install $VIM;
-				fi
-				echo ">> VIm now present <<"
-}
-
-function vim_present()
-{
-				[ -x `which vim` ]
 }
 
 function setup_vimrc()
 {
 				echo ">> Setting up VImrc <<"
 				#First line vimrc
-				sed -i "1i source \"$CONFIGPATH/installplgrc\"" $VIMRC
+				sed -i "1i source $CONFIGPATH/installplgrc" $VIMRC
 
 				#Last line vimrc
-				sed -i "\$asource \"$CONFIGPATH/setupplgrc\"" $VIMRC
+				sed -i "\$asource $CONFIGPATH/setupplgrc" $VIMRC
 				sed -i 's#XMYPLUGINX#Plugin \x27file:///$CONFIGPATH/devplugin\x27#g' config/installplgrc
 }
 
 function setup_plugins()
 {
+				git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 				$VIM $PLGINSTALLOPTS
 }
 
@@ -98,9 +86,3 @@ function for_each_plugin()
 								"$PLUGIN"_install
 				done
 }
-
-install_vim
-setup_vimrc
-mkdir -p $HOME/.vim
-setup_plugins
-for_each_plugin
